765cb7cae54b0603fa4bd1b317b65bea3a158de9
Bergwolf4880 <poprjaduhha.ivan@gmail.com>
2024-02-23 00:31:29 +0100

    classwork & homework

--- front_end/lesson_16/example_01/scripts/index.js
+++ front_end/lesson_16/example_01/scripts/index.js
@@ -14,57 +14,48 @@ let todoList = [
     {task: "Поиграть в видеоигры", done: false, deadline: "2024-02-27"},
     {task: "Послать резюме", done: true, deadline: "2024-02-20"},
     {task: "Поехать в отпуск", done: false, deadline: "2024-02-29"},
-    {task: "Поехать в отпуск", done: false},
+        
   ];
 
- 
-const listItemTwoElement = document.querySelector('list-item');
+const listAllElements = document.querySelector('.list');
 const inputElement = document.querySelector('.task-input');
 const createButton = document.querySelector('.btn');
-let userInputText;
+const showAllButton = document.querySelector('#all');
+const showDoneButton = document.querySelector('#completed');
+const showUndoneButton = document.querySelector('#not_completed');
+
+const updateList = (array) => {                                     //параметром (array) является наш массив
+    listAllElements.innerHTML = '';                                 //стирает весь список который уже есть (если он есть)
+    array.forEach(element => {                                      //и содержимое цикла отвечает за создание списка с нуля
+        const newListItem = document.createElement('li');           //создаётся ячейка (элемент 'li')
+        newListItem.classList.add('list-item');                     //добавляется ячейке (элементу 'li') класс (list-item) 
+        if(element.done){                                           //если 'done' === true
+            newListItem.classList.add('list-item_done');            //присваеваем ячейке (элементу 'li') класс ('list-item_done')
+        }
+        newListItem.textContent = element.task;                     //добавляем текс таска из массива
+        newListItem.addEventListener('click', listItemClickHandler);//включаем функцию зачеркивания тасков. этот лисенер должен находиться в цикле, чтобы применять свои строки для каждого элемента массива
+        listAllElements.append(newListItem);                        //добавляем новый элемент в лист
+    });
+}
 
 const createButtonHandler = () => {
-    const newListItem = document.createElement('li');
-    newListItem.classList.add('list-item');
-    newListItem.textContent = userInputText;
-    const listAllElements = document.querySelector('.list');
-    listAllElements.append(newListItem);
+    const newEntry = {task: inputElement.value, done: false};
+    todoList.push(newEntry);
+    inputElement.value = '';                                        //обнуляет поле ввода после нажатия кнопки создать
+    updateList(todoList);
+    // console.log(todoList);
 }
 
-const arrayLoop = (element) => {
-
-        const newListItem = document.createElement('li');
-        newListItem.classList.add('list-item');
-        newListItem.textContent =  element['task'];
-        if(element['done'] === true){
-            newListItem.classList.add('list-item_done');
-        } else {
-
-        }
-        const listAllElements = document.querySelector('.list');
-        listAllElements.append(newListItem);
-
+const listItemClickHandler = (event) => {                           //отвечает за зачеркивание тасков списка и обратно
+    const taskText = event.target.textContent;                      //переменная taskText будет содержать текст таска на который нажали
+    const task = todoList.find(task => task.task === taskText);     //переменная task будет содержать функцию find(), которая ищет первый элемент с заданным текстом
+    task.done = !task.done;                                         //было true становиться false; было false становиться true
+    updateList(todoList);                                           //обновление списка
 }
 
-todoList.forEach(arrayLoop);
+createButton.addEventListener('click', createButtonHandler);        
+showAllButton.addEventListener('click', () => updateList(todoList));                                //лисенер получает функцию update list для обнуления и создания списка с нуля
+showDoneButton.addEventListener('click', () => updateList(todoList.filter(task => task.done)));     //лисенер получает снова созданный список и сразу фильтрует его, чтобы показать только таски где 'done' === true
+showUndoneButton.addEventListener('click', () => updateList(todoList.filter(task => !task.done)));  //лисенер получает снова созданный список и сразу фильтрует его, чтобы показать только таски где 'done' === !true
 
-
-const listItemTwoHandler = () => {
-    listItemTwoElement.textContent = todoList['done'];
-    listItemTwoElement.classList.toggle('list-item_done');
-    
-}
-
-const inputHandler = () => {
-userInputText = inputElement.value;
-
-}
-
-
-//listItemTwoElement.addEventListener('click', listItemTwoHandler);
-inputElement.addEventListener('input', inputHandler);
-createButton.addEventListener('click', createButtonHandler);
-
-const btnHandler = (evt) => {
-    evt.preventDefault();
-}
+updateList(todoList);
