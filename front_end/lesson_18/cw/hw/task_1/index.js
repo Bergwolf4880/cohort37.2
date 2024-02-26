c9814fe63ce1207029e5af4a8f3b51d38530e01f
Bergwolf4880 <poprjaduhha.ivan@gmail.com>
2024-02-26 22:41:54 +0100

    classwork & homework

--- front_end/lesson_18/cw/hw/task_1/index.js
+++ front_end/lesson_18/cw/hw/task_1/index.js
@@ -1,15 +1,22 @@
 // Написать функцию, которая принимает на вход массив имен и возвращает массив имен,
 //  начинающихся на букву "а" (большую или маленькую).
 
-const names = 
-["Alex", "Bob", "Chris", "David", "Eva", "Frank", "Grace", 
-"Helen", "Ian", "Jack", "Kate", "Leo", "Max", "Nora", "Oscar"];
+const names =
+   ["Bob", "Chris", "David", "Eva", "Frank", "Grace",
+      "Helen", "Ian", "Jack","Alex", "Kate", "Leo", "Max", "Nora", "Oscar"];
 
+
+const filteredNamesOld = (array) => {
+return array.filter(element => element[0].toLowerCase().includes('a'));
+
+}
+
+
+
+const checkFirstAchar = string => string[0].toLowerCase().includes('a')
 
 const filteredNames = (array) => {
-
-return array.filter( element => element[0].toLowerCase().includes('a'));
-
+return array.filter(checkFirstAchar);
 }
 
 console.log(filteredNames(names));
--- front_end/lesson_18/cw/hw/task_2/index.js
+++ front_end/lesson_18/cw/hw/task_2/index.js
@@ -4,6 +4,10 @@
 const numbers = [1,2,3,4,5,6,7,8,9];
 
 function sumAllNumbers(arr){
-    return arr.reduce((i, j) => i+j);}
+    return arr.reduce(summElements);}
+
+
+const summElements = (((i, j) => i+j));
+
 
 console.log(sumAllNumbers(numbers));
